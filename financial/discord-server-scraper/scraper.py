40f06668cfc3437a8bd4bf7d2b29fa4ad051a00f
Alexander Lerma <alexander.lerma@neuralnetes.com>
2020-11-27 17:02:33 -0500

    wip

---
+++ financial/discord-server-scraper/scraper.py
@@ -0,0 +1,25 @@
+import os
+import asyncio
+import discord
+from dotenv import load_dotenv
+
+class DiscordClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        channel = discord.utils.get(self.get_all_channels(), guild__name='stonky-people', name='general')
+        print(channel)
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+async def main():
+    load_dotenv()
+    token = os.environ['DISCORD_BOT_TOKEN']
+    client = DiscordClient()
+    return asyncio.gather(
+        client.start(
+            token,
+            bot=True
+        )
+    )
