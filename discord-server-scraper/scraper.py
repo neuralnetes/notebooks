22f48614b4d7f19b65017d9a95c60bb9bc9aa2c7
Alexander Lerma <alexander.lerma@neuralnetes.com>
2020-11-29 18:28:37 -0500

    wip

--- discord-server-scraper/scraper.py
+++ discord-server-scraper/scraper.py
@@ -7,21 +7,28 @@ from dotenv import load_dotenv
 class DiscordClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
-        # channel = discord.utils.get(self.get_all_channels(), guild__name='stonky-people', name='general')
-        # print(channel)
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+
+
+async def get_channel_history(client: DiscordClient, channel_id: int):
+    channel = await client.get_channel(channel_id)
+    messages = await channel.history().flatten()
+    print(messages)
+    return messages
 
 
 async def main():
     load_dotenv()
     discord_token = os.environ['DISCORD_TOKEN']
     discord_bot = os.environ['DISCORD_BOT'] == 'true'
+    discord_channel_id = 592829914022150146
     client = DiscordClient()
     return asyncio.gather(
         client.start(
             discord_token,
             bot=discord_bot
-        )
+        ),
+        get_channel_history(client, discord_channel_id)
     )
