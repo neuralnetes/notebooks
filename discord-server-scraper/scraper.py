75ff5ff8e75b08406e45235cb289081bfa7f8d5e
Alexander Lerma <alexander.lerma@neuralnetes.com>
2020-11-29 19:04:00 -0500

    wip

--- discord-server-scraper/scraper.py
+++ discord-server-scraper/scraper.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import discord
 from dotenv import load_dotenv
+from collections import defaultdict
 
 
 class DiscordClient(discord.Client):
@@ -12,21 +13,30 @@ class DiscordClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
 
-async def get_channel_history(client: DiscordClient, channel_id: int):
-    channel = await client.get_channel(channel_id)
+async def fetch_channel_messages(client: DiscordClient, channel_id: int):
+    channel = await client.fetch_channel(channel_id)
     messages = await channel.history().flatten()
-    print(messages)
     return messages
+
+
+def group_message_content_by_author(messages):
+    grouped = defaultdict(list)
+    for message in messages:
+        grouped[message.author.name].append(message.content)
+    return grouped
 
 
 async def main():
     load_dotenv()
     discord_token = os.environ['DISCORD_TOKEN']
     discord_bot = os.environ['DISCORD_BOT'] == 'true'
+    # discord_guild_id = 542224582317441034
     discord_channel_id = 592829914022150146
     client = DiscordClient()
-    _ = await client.start(
+    await client.login(
         discord_token,
         bot=discord_bot
     )
-    return get_channel_history(client, discord_channel_id)
+    messages = await fetch_channel_messages(client, discord_channel_id)
+    grouped_by_author = group_message_content_by_author(messages)
+    print(grouped_by_author)
